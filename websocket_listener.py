a3035487b6dda343216418e9d49bbd6140e13c1c
Nalivkin8 <nalivkin@bk.ru>
2025-03-16 15:42:44 +0400

    Update websocket_listener.py

--- websocket_listener.py
+++ websocket_listener.py
@@ -1,23 +1,28 @@
 import websocket
 import json
+import asyncio
 
-def on_message(ws, message):
-    data = json.loads(message)
-    price = float(data['p'])
-    print(f"🔥 Текущая цена BTC/USDT: {price}")
+# Подключение к WebSocket Binance
+def start_websocket(bot, chat_id):
+    def on_message(ws, message):
+        data = json.loads(message)
+        price = float(data['p'])
+        asyncio.run(send_price(bot, chat_id, price))
 
-def on_open(ws):
-    subscribe_message = json.dumps({
-        "method": "SUBSCRIBE",
-        "params": ["btcusdt@trade"],
-        "id": 1
-    })
-    ws.send(subscribe_message)
+    def on_open(ws):
+        subscribe_message = json.dumps({
+            "method": "SUBSCRIBE",
+            "params": ["btcusdt@trade"],
+            "id": 1
+        })
+        ws.send(subscribe_message)
 
-def start_websocket():
     ws = websocket.WebSocketApp(
         "wss://stream.binance.com:9443/ws/btcusdt@trade",
         on_message=on_message,
         on_open=on_open
     )
     ws.run_forever()
+
+async def send_price(bot, chat_id, price):
+    await bot.send_message(chat_id, f"🔥 Текущая цена BTC/USDT: {price}")
